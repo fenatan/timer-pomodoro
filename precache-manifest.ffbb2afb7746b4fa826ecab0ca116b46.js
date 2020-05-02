4e618a1f3a37e843fd4c3b5ac975f8ad2129548c
Felipe Natan <felipenatan@novaandradina.org>
2020-05-01 22:29:41 -0400

    Updates

---
+++ precache-manifest.ffbb2afb7746b4fa826ecab0ca116b46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380db69cb6179e48460cf4c33430bfed",
+    "url": "/timer-pomodoro/index.html"
+  },
+  {
+    "revision": "c3d1a4b7d89294cde91e",
+    "url": "/timer-pomodoro/static/css/main.28841a41.chunk.css"
+  },
+  {
+    "revision": "0e56d8db9d1733675305",
+    "url": "/timer-pomodoro/static/js/2.4634fe6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/timer-pomodoro/static/js/2.4634fe6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d1a4b7d89294cde91e",
+    "url": "/timer-pomodoro/static/js/main.884bb769.chunk.js"
+  },
+  {
+    "revision": "6470c6fbd53274e84f72",
+    "url": "/timer-pomodoro/static/js/runtime-main.97661d5f.js"
+  },
+  {
+    "revision": "d82c39fd5aa149573ec9366082b6a2f6",
+    "url": "/timer-pomodoro/static/media/end-time.d82c39fd.wav"
+  },
+  {
+    "revision": "20f930b5c81597710cf237423bee1394",
+    "url": "/timer-pomodoro/static/media/pause.20f930b5.wav"
+  },
+  {
+    "revision": "dc14e1493146d10ad706fd35497874c7",
+    "url": "/timer-pomodoro/static/media/reset.dc14e149.wav"
+  },
+  {
+    "revision": "872073a1918e0ac80adf92bdfd2fa67b",
+    "url": "/timer-pomodoro/static/media/start.872073a1.wav"
+  }
+]);
